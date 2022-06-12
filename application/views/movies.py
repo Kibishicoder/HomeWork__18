079511de543df926f0e11358b6729b950f3a94cd
Kibishicoder <kibishiart@gmail.com>
2022-06-12 21:25:49 +0300

    Initial commit

---
+++ application/views/movies.py
@@ -0,0 +1,43 @@
+from flask import request
+from flask_restx import Resource, Namespace
+from marshmallow import ValidationError
+
+from application.services.schemas.movie import MovieSchema
+from application.container import movies_service
+
+movies_ns = Namespace('movies')
+movie_schema = MovieSchema()
+
+
+@movies_ns.route('/<int:uid>')
+class MovieView(Resource):
+
+    def get(self, uid):
+        return movies_service.get_one(uid)
+
+    def put(self, uid):
+        try:
+            return movies_service.patch(uid, request.json), 204
+        except ValidationError as e:
+            return {'errors': e.normalized_messages()}, 400
+
+
+@movies_ns.route('/')
+class MoviesView(Resource):
+
+    def get(self):
+        args = request.args
+
+        if 'director_id' in args:
+            return movies_service.get_by_director_id(args['director_id'])
+
+        if 'genre_id' in args:
+            return movies_service.get_by_genre_id(args['genre_id'])
+
+        if 'year' in args:
+            return movies_service.get_by_year(args['year'])
+
+        return movies_service.get_all()
+
+    def post(self):
+        return movies_service.post(request.json)
